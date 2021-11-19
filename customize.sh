11bf6d3134b4e9d53bc10593d15134878da14b8d
muink <hukk1996@gmail.com>
2021-11-20 02:11:30 +0800

    Add more Captive Portal URL
    123

--- customize.sh
+++ customize.sh
@@ -42,28 +42,63 @@ if keytest; then
 else
     FUNC=false
     ui_print "   ! Legacy device detected! Setting module to default state (1). You can change it in $MODPATH/service.sh !"
-    ui_print "   ! Setting MCC_MODE to 0 will disable CPD completely, while setting it to 1 will just change the Google URL to Kuketz !"
+    ui_print "   ! Setting MCC_MODE to 0 will disable CPD completely, while setting it to 1 will change the Google URL to MSFTEdge or Cloudflare!"
+    ui_print "   ! Setting MCC_ADDR to 0 will keep the default Google Captive Portal URL, while setting it to 'msftedge' will change the Google URL to MSFTEdge, and setting it to 'cloudflare' will change the Google URL to Cloudflare !"
 fi
 
 # Ask user for Captive Portal Detection mode
 # ------------------------------------------
 ui_print " "
 ui_print "--- Select Captive Portal Detection mode ---"
-ui_print "  Vol+ = Change to Kuketz.de  "
-ui_print "  Vol- = Completely disable  "
+ui_print "  Vol+ = Completely disable CPD            "
+ui_print "  Vol- = Change Google URL or Restore URL  "
 ui_print " "
 
 if "$FUNC"; then
-    MCC_MODE=0
-    ui_print "Selected: Completely disable Captive Portal Detection"
-else
     MCC_MODE=1
-    ui_print "Selected: Change the Captive Portal Detection URL to Kuketz.de"
+    MCC_ADDR=0
+    ui_print "Selected: Change the Captive Portal Detection URL to other than Google or Restore default URL"
+else
+    MCC_MODE=0
+    MCC_ADDR=0
+    ui_print "Selected: Completely disable Captive Portal Detection"
+fi
+
+
+if [ "$MCC_MODE" -eq 1 ]; then
+# Ask user if wants to restore default
+# ------------------------------------
+ui_print " "
+ui_print "--- Need to restore the default ? ---"
+ui_print "  Vol+ = Yes  "
+ui_print "  Vol- = No   "
+ui_print " "
+
+if "$FUNC"; then
+    # Ask user for Captive Portal Detection address
+    # ---------------------------------------------
+    ui_print " "
+    ui_print "--- Select Captive Portal Detection address ---"
+    ui_print " "
+
+    for _key in msftedge cloudflare qualcomm kuketz; do
+        ui_print " "
+        ui_print "  Change to $_key  "
+        ui_print "  Vol+ = Yes , Vol- = No  "
+        ui_print " "
+        if ! "$FUNC"; then
+            MCC_ADDR="$_key"
+            ui_print "Selected: Change the Captive Portal Detection URL to $_key"
+            break
+        fi
+    done
+
+fi
 fi
 
 ui_print " "
-ui_print "Writing Captive Portal Detection mode to startup script…  "
-sed -i "s/<DUMMY>/$MCC_MODE/g" "$MODPATH/service.sh"
+ui_print "Writing Captive Portal Detection mode and address to startup script…  "
+sed -i "s/<DUMMY>/$MCC_MODE/g; s/<ADDRS>/$MCC_ADDR/g" "$MODPATH/service.sh"
 
 # Just in case, make startup script executable
 # --------------------------------------------
--- service.sh
+++ service.sh
@@ -12,22 +12,58 @@
 #!/system/bin/sh
 
 # Mode: 1 — Just change the Captive Portal URL, 0 — Completely disable Captive Portal detection
+# Addr: 0 — Use the default Google, msftedge — Use the MSFT Edge, cloudflare — Use the Cloudflare
 # ---------------------------------------------------------------------------------------------
 MCC_MODE=<DUMMY>
+MCC_ADDR=<ADDRS>
 
-# Always set the Captive Portal URL to other than Google
-# ------------------------------------------------------
-settings put global captive_portal_http_url "http://captiveportal.kuketz.de"
-settings put global captive_portal_https_url "https://captiveportal.kuketz.de"
-settings put global captive_portal_fallback_url "http://captiveportal.kuketz.de"
-settings put global captive_portal_other_fallback_urls "http://captiveportal.kuketz.de"
+msftedge='edge.microsoft.com/captiveportal/generate_204'
+cloudflare='cp.cloudflare.com/generate_204'
+qualcomm='www.qualcomm.cn/generate_204'
+kuketz='captiveportal.kuketz.de'
+
+# Set the Captive Portal URL to other than Google
+# -----------------------------------------------
+#settings put global captive_portal_http_url "http://"
+#settings put global captive_portal_https_url "https://"
+#settings put global captive_portal_fallback_url "http://"
+#settings put global captive_portal_other_fallback_urls "http://"
+
+case "$MCC_ADDR" in
+    0)
+      settings delete global captive_portal_http_url
+      settings delete global captive_portal_https_url
+    ;;
+    msftedge)
+      settings put global captive_portal_http_url "http://${msftedge}"
+      settings put global captive_portal_https_url "https://${msftedge}"
+    ;;
+    cloudflare)
+      settings put global captive_portal_http_url "http://${cloudflare}"
+      settings put global captive_portal_https_url "https://${cloudflare}"
+    ;;
+    qualcomm)
+      settings put global captive_portal_http_url "http://${qualcomm}"
+      settings put global captive_portal_https_url "https://${qualcomm}"
+    ;;
+    kuketz)
+      settings put global captive_portal_http_url "http://${kuketz}"
+      settings put global captive_portal_https_url "https://${kuketz}"
+    ;;
+    *) >/dev/null echo ? ;;
+esac
 
 # Disable Captive Portal Detection if it is desired
 # -------------------------------------------------
 if [ "$MCC_MODE" -eq 0 ]; then
-    settings put global captive_portal_detection_enabled 0
     settings put global captive_portal_server localhost
+    settings put global captive_portal_detection_enabled 0
     settings put global captive_portal_mode 0
+else
+    settings delete global captive_portal_server
+    settings delete global captive_portal_detection_enabled
+    settings delete global captive_portal_mode
 fi
 
+# https://android.stackexchange.com/questions/186993/captive-portal-parameters
 exit 0
